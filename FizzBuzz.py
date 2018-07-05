aa938daa46b237c61cbd8ec233d0d5abd486bd35
carlag <carlagouws88@gmail.com>
2018-07-05 09:35:08 +0100

    Added FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+number = 100
+
+for i in range(number+1):
+    if i == 0:
+        continue
+    elif i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
