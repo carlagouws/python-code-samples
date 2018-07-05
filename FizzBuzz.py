8fdb7d2fa719805c8cfadf53d5ab613f2bf683a9
carlag <carlagouws88@gmail.com>
2018-07-05 09:42:27 +0100

    Updated FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,7 @@
 number = 100
 
-for i in range(number+1):
-    if i == 0:
-        continue
-    elif i % 3 == 0 and i % 5 == 0:
+for i in range(1,number+1):
+    if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
     elif i % 3 == 0:
         print("Fizz")
